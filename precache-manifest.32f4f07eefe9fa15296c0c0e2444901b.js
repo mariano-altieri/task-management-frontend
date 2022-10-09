4645f4e337d4bc545517a6eab9714430bfc8eb7b
mariano-altieri <mariano-altieri@users.noreply.github.com>
2022-10-09 21:57:54 +0000

    deploy: 9d0aa21454c9b203bc0e0dfcf4175a5650ff2a4d

---
+++ precache-manifest.32f4f07eefe9fa15296c0c0e2444901b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5214106330159463df21618c4537155",
+    "url": "https://mariano-altieri.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "06ef2b30d0bbe2ca1883",
+    "url": "https://mariano-altieri.github.io/task-management-frontend/static/css/main.221589fe.chunk.css"
+  },
+  {
+    "revision": "a2a04b74187481eaa957",
+    "url": "https://mariano-altieri.github.io/task-management-frontend/static/js/2.22e84243.chunk.js"
+  },
+  {
+    "revision": "06ef2b30d0bbe2ca1883",
+    "url": "https://mariano-altieri.github.io/task-management-frontend/static/js/main.4c82b85b.chunk.js"
+  },
+  {
+    "revision": "502e97a020dd26eb840f",
+    "url": "https://mariano-altieri.github.io/task-management-frontend/static/js/runtime~main.cc842a1f.js"
+  }
+]);
