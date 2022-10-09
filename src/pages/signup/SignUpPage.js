9d0aa21454c9b203bc0e0dfcf4175a5650ff2a4d
Mariano Altieri <marianoa@givingassistant.org>
2022-10-09 18:55:57 -0300

    Fix sass issues

--- src/pages/signup/SignUpPage.js
+++ src/pages/signup/SignUpPage.js
@@ -1,98 +1,105 @@
-import React, { Component } from 'react';
-import { Button, TextField } from '@material-ui/core';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import { Button, TextField } from "@material-ui/core";
+import styled from "styled-components";
 
-import './SignUpPage.scss';
-import { inject } from 'mobx-react';
-import ErrorMessage from '../../components/ErrorMessage';
+import "./SignUpPage.css";
+import { inject } from "mobx-react";
+import ErrorMessage from "../../components/ErrorMessage";
 
 const Heading = styled.h1`
-  margin-top: 0;
+    margin-top: 0;
 `;
 
 const FormContainer = styled.div`
-  max-width: 480px;
-  width: 100%;
-  background-color: #edf4ff;
-  padding: 30px;
-  border-radius: 5px;
+    max-width: 480px;
+    width: 100%;
+    background-color: #edf4ff;
+    padding: 30px;
+    border-radius: 5px;
 `;
 
 const FormField = styled(TextField)`
-  width: 100%;
+    width: 100%;
 `;
 
-@inject('userStore', 'routerStore')
+@inject("userStore", "routerStore")
 class SignUpPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-      password: '',
-      errorMessage: null,
-    };
-  }
-
-  submit = async () => {
-    const { username, password } = this.state;
-
-    try {
-      await this.props.userStore.signup(username, password);
-      window.location.hash = '/signin';
-    } catch (error) {
-      const errorMessage = error.response.data.message;
-      this.setState({ errorMessage });
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            password: "",
+            errorMessage: null,
+        };
     }
-  };
 
-  render() {
-    const { errorMessage } = this.state;
+    submit = async () => {
+        const { username, password } = this.state;
 
-    return (
-      <div className="fullscreen-wrapper">
-        <FormContainer>
-          <Heading>Join us!</Heading>
-          <p>Start managing tasks easily.</p>
+        try {
+            await this.props.userStore.signup(username, password);
+            window.location.hash = "/signin";
+        } catch (error) {
+            const errorMessage = error.response.data.message;
+            this.setState({ errorMessage });
+        }
+    };
 
-          {errorMessage && <ErrorMessage message={this.state.errorMessage} />}
+    render() {
+        const { errorMessage } = this.state;
 
-          <div>
-            <FormField
-              id="outlined-name"
-              label="Username"
-              margin="dense"
-              variant="outlined"
-              onChange={e => this.setState({ username: e.target.value })}
-            />
-          </div>
-          <div>
-            <FormField
-              id="outlined-name"
-              label="Password"
-              margin="dense"
-              variant="outlined"
-              type="password"
-              onChange={e => this.setState({ password: e.target.value })}
-            />
-          </div>
-          <p>
-            Passwords must contain at least 1 upper case letter, 1 lower case letter and one number OR special charracter.
-          </p>
-          <hr/>
-          <div>
-            <Button
-              fullWidth
-              variant="contained"
-              color="primary"
-              onClick={this.submit}
-            >
-              SIGN UP
-            </Button>
-          </div>
-        </FormContainer>
-      </div>
-    );
-  }
+        return (
+            <div className="fullscreen-wrapper">
+                <FormContainer>
+                    <Heading>Join us!</Heading>
+                    <p>Start managing tasks easily.</p>
+
+                    {errorMessage && (
+                        <ErrorMessage message={this.state.errorMessage} />
+                    )}
+
+                    <div>
+                        <FormField
+                            id="outlined-name"
+                            label="Username"
+                            margin="dense"
+                            variant="outlined"
+                            onChange={(e) =>
+                                this.setState({ username: e.target.value })
+                            }
+                        />
+                    </div>
+                    <div>
+                        <FormField
+                            id="outlined-name"
+                            label="Password"
+                            margin="dense"
+                            variant="outlined"
+                            type="password"
+                            onChange={(e) =>
+                                this.setState({ password: e.target.value })
+                            }
+                        />
+                    </div>
+                    <p>
+                        Passwords must contain at least 1 upper case letter, 1
+                        lower case letter and one number OR special charracter.
+                    </p>
+                    <hr />
+                    <div>
+                        <Button
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            onClick={this.submit}
+                        >
+                            SIGN UP
+                        </Button>
+                    </div>
+                </FormContainer>
+            </div>
+        );
+    }
 }
 
 export default SignUpPage;
